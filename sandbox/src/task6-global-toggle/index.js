3178d5303d89b7f5c5463307e8953206f47b5628
king3228 <miroshin001@gmail.com>
2024-10-27 16:25:52 +0300

    task 6 complete

--- sandbox/src/task6-global-toggle/index.js
+++ sandbox/src/task6-global-toggle/index.js
@@ -5,5 +5,22 @@
  * @param {String} className Название класса
  */
 export const globalToggle = (className) => {
-    // Ваш код здесь
+    // Найти все элементы с переданным классом и элементы с активным вариантом этого класса
+    const elements = document.querySelectorAll(`.${className}, .${className}_active`);
+
+    // Если ни одного элемента не найдено, прекращаем работу
+    if (elements.length === 0) {
+        return;
+    }
+
+    // Меняем класс для всех найденных элементов
+    elements.forEach(element => {
+        if (element.classList.contains(`${className}_active`)) {
+            element.classList.remove(`${className}_active`);
+            element.classList.add(className);
+        } else if (element.classList.contains(className)) {
+            element.classList.remove(className);
+            element.classList.add(`${className}_active`);
+        }
+    });
 };
