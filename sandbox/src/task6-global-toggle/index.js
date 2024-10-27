585b215a9bee4a01e76f34011ade48a4d103c2d1
king3228 <miroshin001@gmail.com>
2024-10-27 16:32:13 +0300

    task 6 fix, 7 complete

--- sandbox/src/task6-global-toggle/index.js
+++ sandbox/src/task6-global-toggle/index.js
@@ -5,15 +5,12 @@
  * @param {String} className Название класса
  */
 export const globalToggle = (className) => {
-    // Найти все элементы с переданным классом и элементы с активным вариантом этого класса
     const elements = document.querySelectorAll(`.${className}, .${className}_active`);
 
-    // Если ни одного элемента не найдено, прекращаем работу
     if (elements.length === 0) {
         return;
     }
 
-    // Меняем класс для всех найденных элементов
     elements.forEach(element => {
         if (element.classList.contains(`${className}_active`)) {
             element.classList.remove(`${className}_active`);
--- sandbox/src/task7-hit-or-run/index.js
+++ sandbox/src/task7-hit-or-run/index.js
@@ -7,5 +7,19 @@
  * @returns {('hit' | 'run')}
  */
 export const hitOrRun = (a, b) => {
-    // Ваш код здесь
+    if (a > b) {
+        throw new Error("a должно быть меньше или равно b");
+    }
+
+    const randomNum = Math.floor(Math.random() * (b - a + 1)) + a;
+
+    function isPrime(num) {
+        if (num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    return isPrime(randomNum) ? 'run' : 'hit';
 };
