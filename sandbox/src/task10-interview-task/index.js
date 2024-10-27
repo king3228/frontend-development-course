760134123fecd4394688e5b72f213d6a7fdac298
king3228 <miroshin001@gmail.com>
2024-10-27 16:54:27 +0300

    task 10 complete

--- sandbox/src/task10-interview-task/index.js
+++ sandbox/src/task10-interview-task/index.js
@@ -6,5 +6,17 @@
  * @returns {Number}
  */
 export const solutionFn = (num) => {
-    // Ваш код здесь
+    if (num < 10) {
+        return num;
+    }
+
+    function multiplyDigits(num) {
+        return num.toString().split('').reduce((product, digit) => product * parseInt(digit, 10), 1);
+    }
+
+    while (num >= 10) {
+        num = multiplyDigits(num);
+    }
+
+    return num;
 };
